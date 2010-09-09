01c85d1e67beca4e6e112dbca0f8e9145b651f17
Mikael Koskinen <mijapkos@gmail.com>
2010-09-09 18:54:16 +0300

    Sample application of changing the project reference based on the Visual Studio build configuration

---
+++ change-reference-based-on-build-configuration/src/MyForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using testLibrary;
+
+namespace change_reference_based_on_build_configuration
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var testClass = new MyTestClass();
+
+            var message = string.Format("Version {0} of external assembly", testClass.MyVersion());
+            
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK);
+        }
+    }
+}
---
+++ change-reference-based-on-build-configuration/src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace change_reference_based_on_build_configuration
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyForm());
+        }
+    }
+}
