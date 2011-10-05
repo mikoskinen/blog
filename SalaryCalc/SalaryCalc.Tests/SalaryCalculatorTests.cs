693f954fd56d9992eef2a6519b748fd2c8942a99
Mikael Koskinen <mijapkos@gmail.com>
2011-10-05 18:34:41 +0300

    Salary calculator

---
+++ SalaryCalc/SalaryCalc.Tests/SalaryCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalc.Tests
+{
+    [TestClass]
+    public class SalaryCalculatorTests
+    {
+        [TestMethod]
+        public void CanCalculateSalary()
+        {
+            const int baseSalary = 1500;
+            const double salaryPerSecond = (double)baseSalary / 2678400;
+
+            var result = SalaryCalculator.CalculateSalaryDataByMonthlySalary(baseSalary);
+
+            Assert.AreEqual(result.PerMonth, baseSalary);
+            Assert.AreEqual(result.PerYear, baseSalary * 12);
+
+            Assert.AreEqual(result.PerSecond, salaryPerSecond);
+            Assert.AreEqual(result.PerMinute, salaryPerSecond * 60);
+            Assert.AreEqual(result.PerHour, salaryPerSecond * 60 * 60);
+            Assert.AreEqual(result.PerDay, salaryPerSecond * 60 * 60 * 24);
+            Assert.AreEqual(result.PerWeek, salaryPerSecond * 60 * 60 * 24 * 7);
+        }
+    }
+}
---
+++ SalaryCalc/SalaryCalc/SalaryCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SalaryCalc
+{
+    public class SalaryCalculator
+    {
+        public static SalaryData CalculateSalaryDataByMonthlySalary(double yourMonthlySalary)
+        {
+            var result = new SalaryData { PerMonth = yourMonthlySalary };
+
+            var secondsInMonth = TimeSpan.FromDays(31).TotalSeconds;
+
+            result.PerSecond = yourMonthlySalary / secondsInMonth;
+            result.PerMinute = result.PerSecond * 60;
+            result.PerHour = result.PerMinute * 60;
+            result.PerDay = result.PerHour * 24;
+            result.PerWeek = result.PerDay * 7;
+            result.PerYear = result.PerMonth * 12;
+
+            return result;
+        }
+    }
+
+    public class SalaryData
+    {
+        public double PerSecond { get; set; }
+        public double PerMinute { get; set; }
+        public double PerHour { get; set; }
+        public double PerDay { get; set; }
+        public double PerWeek { get; set; }
+        public double PerMonth { get; set; }
+        public double PerYear { get; set; }
+    }
+}
