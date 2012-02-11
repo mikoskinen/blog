a614666555cd3b4b98c84777a599c177dab02fa5
Mikael Koskinen <mijapkos@gmail.com>
2012-02-11 18:49:02 +0200

    Unity constructor parameters resolve

---
+++ unity-custom-constructor-parameters/TestSubject/MyClass.cs
@@ -0,0 +1,14 @@
+﻿namespace TestSubject
+{
+    public class MyClass
+    {
+        public string Hello { get; set; }
+        public int Number { get; set; }
+
+        public MyClass(string hello, int number)
+        {
+            Hello = hello;
+            Number = number;
+        }
+    }
+}
---
+++ unity-custom-constructor-parameters/TestsAndCustomResolverOverride/Tests.cs
@@ -0,0 +1,37 @@
+﻿using TestSubject;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+
+namespace ClassLibrary2
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestParameterOverride()
+        {
+            var unity = new UnityContainer();
+            unity.RegisterType<MyClass>();
+
+            var myObj = unity.Resolve<MyClass>(new ResolverOverride[]
+                                           {
+                                               new ParameterOverride("hello", "hi there"), new ParameterOverride("number", 21)
+                                           });
+
+            Assert.That(myObj.Hello, Is.EqualTo("hi there"));
+            Assert.That(myObj.Number, Is.EqualTo(21));
+        }
+
+        [Test]
+        public void TestOrderedParametersOverride()
+        {
+            var unity = new UnityContainer();
+            unity.RegisterType<MyClass>();
+
+            var myObj = unity.Resolve<MyClass>(new OrderedParametersOverride(new object[] {"greetings", 24 }));
+
+            Assert.That(myObj.Hello, Is.EqualTo("greetings"));
+            Assert.That(myObj.Number, Is.EqualTo(24));
+        }
+    }
+}
