fb1e167c7623cbe5efbcd4a88b0926ae4a8dc433
Mikael Koskinen <mijapkos@gmail.com>
2013-03-21 13:00:05 +0200

    startup project example

---
+++ wp7-wp8-Startup-project/AppStartup.wp7/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace AppStartup.wp7
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/MainProject;component/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ wp7-wp8-Startup-project/PhoneApp65/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace MainProject
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (e.NavigationMode == NavigationMode.New)
+                NavigationService.RemoveBackEntry();
+        }
+    }
+}
